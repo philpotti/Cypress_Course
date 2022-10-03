ca61304869dd1e4c020a98c341bbbfedf91cd97d
philpotti <imhaslop@gmail.com>
2022-10-03 12:26:05 -0300

    Cypress Automation v1.3

--- cypress/integration/examples/12th Mouse Hover.js
+++ cypress/integration/examples/12th Mouse Hover.js
@@ -1,5 +1,11 @@
 /// <reference types="Cypress" />
 
 describe("My Twelfth Test Case - Mouse Hover", () => {
-  it("Mouse Hover", () => {});
+  it("Mouse Hover", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+    cy.get("#mousehover").invoke("show");
+    cy.contains("Top").click({ force: true }); // {force: true} helps to click on NON VISIBLE objects
+    cy.url().should("include", "/#top");
+  });
 });
